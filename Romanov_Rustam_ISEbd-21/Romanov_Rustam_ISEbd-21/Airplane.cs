957a542d024395fd17a7bbcb4972b38b2d716efd
GUSTAW1R <yamantaw@mail.ru>
2018-11-03 13:53:58 +0400

    Доработана Лабораторная №1

--- Romanov_Rustam_ISEbd-21/Romanov_Rustam_ISEbd-21/Airplane.cs
+++ Romanov_Rustam_ISEbd-21/Romanov_Rustam_ISEbd-21/Airplane.cs
@@ -7,13 +7,7 @@ using System.Threading.Tasks;
 
 namespace Romanov_Rustam_ISEbd_21
 {
-    public enum Direction
-    {
-        Up,
-        Down,
-        Left,
-        Right
-    }
+    
     class Airplane
     {
         private int _startPosX;
